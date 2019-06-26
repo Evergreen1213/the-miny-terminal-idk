28a160bbf76565ff86b719ca9d86ddd08d47a839
Evergreen1213 <48633606+Evergreen1213@users.noreply.github.com>
2019-06-26 14:04:40 -0400

    Update download_and_run_the_github.bash
    added some more intuitive options for updating/downloading the GitHub repository

--- download_and_run_the_github.bash
+++ download_and_run_the_github.bash
@@ -1,1 +1,35 @@
-git clone https://github.com/Evergreen1213/the-miny-terminal-idk.git
+echo "would you like to update an old version or download one"
+
+while [ true ]; do
+    read dat_input
+    echo "    ╭────────────╮
+        │u = update  │
+        │d = download│
+        ╰────────────╯"
+    if [ "$dat_input" = 'u' ] || [ "$dat_input" = 'update']; then
+        while true; do
+            echo drag the file into this terminal window and press ENTER.
+            read update_file_directry
+            if [[ -f "$update_file_directry" ]]
+            then
+                echo "file exists"
+                break
+            else
+                echo "file not found try again"
+            fi
+        done
+        
+    elif [ "$dat_input" = 'd' ] || [ "$dat_input" = 'download']; then
+        echo drag the folder where you want the file to download into this terminal window
+        read download_folder
+        cd ~$download_folder
+        git clone https://github.com/Evergreen1213/the-miny-terminal-idk2.git
+        bash "/the-miny-terminal-idk2/download_and_run_the_github.bash"
+        break
+
+    else
+        echo "choose an option please:"
+    fi
+done
+
+#git clone https://github.com/Evergreen1213/the-miny-terminal-idk2.git; bash "/the-miny-terminal-idk2/download_and_run_the_github.bash"
