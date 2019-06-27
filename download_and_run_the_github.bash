e0b1b1048ca4c71a3098ca4a2f30d41344034ef9
Evergreen1213 <48633606+Evergreen1213@users.noreply.github.com>
2019-06-27 14:30:18 -0400

    update to actually update when asked to update

--- download_and_run_the_github.bash
+++ download_and_run_the_github.bash
@@ -1,33 +1,54 @@
 echo "would you like to update an old version or download one"
 
 while [ true ]; do
-    read dat_input
     echo "    ╭────────────╮
-        │u = update  │
-        │d = download│
-        ╰────────────╯"
+    │u = update  │
+    │d = download│
+    ╰────────────╯"
+    read dat_input
+
     if [ "$dat_input" = 'u' ] || [ "$dat_input" = 'update']; then
         while true; do
             echo drag the file into this terminal window and press ENTER.
             read update_file_directry
-            if [[ -f "$update_file_directry" ]]; then
-                echo "file exists"
+            if [[ -d "$update_file_directry" ]]; then
+                echo "folder exists"
+                cd "$update_file_directry"
+                cd ..
+                pwd
+                while true; do
+                    echo "which version would you like
+                    ╭───────────────────╮
+                    │m = master (stable)│
+                    │b = beta (unstable)│
+                    ╰───────────────────╯"
+                    read version
+                    if [ $version = b ]; then
+                        
+                        rm -rfv "$update_file_directry"
+                        git clone -b "beta_version-(experimental)" https://github.com/Evergreen1213/the-miny-terminal-idk2.git
+                        break
+                    elif [ $version = m ]; then
+                        rm -rfv "$update_file_directry"
+                        git clone https://github.com/Evergreen1213/the-miny-terminal-idk2.git
+                        break
+                    else
+                        echo "choose an option please:"
+                    fi
+                done
+
                 break
             else
-                echo "file not found try again"
+                echo "folder not found, try again"
             fi
         done
-        
     elif [ "$dat_input" = 'd' ] || [ "$dat_input" = 'download']; then
         echo drag the folder where you want the file to download into this terminal window
         read download_folder
         cd $download_folder
-        
         git clone https://github.com/Evergreen1213/the-miny-terminal-idk2.git
         bash "$download_folder/the-miny-terminal-idk2/download_and_run_the_github.bash"
-
         break
-
     else
         echo "choose an option please:"
     fi
