eeb053649778fc8efc13d07548be861255637f29
Evergreen1213 <48633606+Evergreen1213@users.noreply.github.com>
2019-06-28 16:47:19 -0400

    created a new game
    cave explorer

---
+++ Cave_explorer.bash
@@ -0,0 +1,67 @@
+for((j=0;j<7;j++)); do
+    echo -ne "^
+    press here to start
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    \r"
+    sleep .5
+    echo -ne "˰
+    press here to start
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    \r"
+    sleep .5
+done
+
+clear
+for((i=0;i<2;i++)); do
+    echo -ne 'Welcome to.  \r'
+    sleep 1
+    echo -ne 'Welcome to.. \r'
+    sleep 1
+    echo -ne 'Welcome to...\r'
+    sleep 1
+done
+echo -ne 'Welcome to.  \r'
+sleep 1
+echo -ne 'Welcome to.. \r'
+sleep 0.3
+
